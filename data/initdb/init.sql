e20b0351a5c82455752afb4a0f7eb6a764e98453
pallgree <hungvt164@gmail.com>
2023-02-26 10:40:29 +0700

    first commit

---
+++ data/initdb/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE "graph-node-bsc";
+CREATE DATABASE "graph-node-bsc-testnet";
+
+GRANT ALL PRIVILEGES ON DATABASE graph-node-bsc TO postgres;
+
+GRANT ALL PRIVILEGES ON DATABASE graph-node-bsc-testnet TO postgres;
