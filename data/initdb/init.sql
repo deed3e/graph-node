76a9d725945da844fe28d452d37c51552d237c4c
deed3e <hungvt164@gmail.com>
2023-11-16 14:44:32 +0700

    update

--- data/initdb/init.sql
+++ data/initdb/init.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE "graph-node-bsc";
-CREATE DATABASE "graph-node-bsc-testnet";
+CREATE DATABASE "graph-node-linea";
+CREATE DATABASE "graph-node-linea-testnet";
 
-GRANT ALL PRIVILEGES ON DATABASE graph-node-bsc TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE graph-node-linea TO postgres;
 
-GRANT ALL PRIVILEGES ON DATABASE graph-node-bsc-testnet TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE graph-node-linea-testnet TO postgres;
